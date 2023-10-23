c31a042e233d9329552446682998fc3cbcf2dca5
Juan David Roa Moyano <jd.roa@uniandes.edu.co>
2023-10-22 20:25:41 -0500

    Taller 2

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ import { Serie } from "./serie.js";
 import { Data } from "./data.js";
 
 let seriesTable:HTMLElement = document.getElementById("series")!;
+let averageLabel:HTMLElement = document.getElementById("average")!;
 
 mostrarSeries(Data);
 
@@ -11,14 +12,38 @@ function mostrarSeries(datos: Serie[]): void {
     for(let serie of datos){
         let trElement: HTMLElement = document.createElement("tr");
         trElement.innerHTML=`<th scope="row">${serie.orden}</th>
-        <td>${serie.name}</td>
+        <td style="color: blue; text-decoration: underline;">${serie.name}</td>
         <td>${serie.channel}</td>
         <td>${serie.seasons}</td>`;
         seriesTbody.appendChild(trElement);
         average += serie.seasons;
     }
     seriesTable.appendChild(seriesTbody);
-    let h2Element: HTMLElement = document.createElement("h2");
-    h2Element.textContent=`Promedio de temporadas: ${average/datos.length}`;
-    document.body.appendChild(h2Element);
+    let pElement: HTMLElement = document.createElement("p");
+    pElement.textContent=`Promedio de temporadas: ${average/datos.length}`;
+    averageLabel.appendChild(pElement);
 }
+
+
+function mostrarInfo(serie:Serie) {
+    const serieInfo = document.getElementById('info');
+    if (serieInfo) {
+        const cardInfo = `<div class="card">
+                            <img src=${serie.image} class="card-img-top">
+                                <div class="card-body">
+                                <h2 class="card-title">${serie.name}</h5>
+                                <p class="card-text">${serie.description}</p>
+                                <a href="${serie.link}" class="btn btn-primary">Ir a la web</a>
+                            </div>
+                        </div>`;
+        serieInfo.innerHTML = cardInfo;
+    }
+}
+const seriesNombres = document.querySelectorAll('#series td:nth-child(2)');
+
+seriesNombres.forEach((nombre, index) => {
+  nombre.addEventListener('click', () => {
+    mostrarInfo(Data[index]);
+  });
+});
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 import { Data } from "./data.js";
 var seriesTable = document.getElementById("series");
+var averageLabel = document.getElementById("average");
 mostrarSeries(Data);
 function mostrarSeries(datos) {
     var average = 0;
@@ -7,12 +8,25 @@ function mostrarSeries(datos) {
     for (var _i = 0, datos_1 = datos; _i < datos_1.length; _i++) {
         var serie = datos_1[_i];
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td>".concat(serie.orden, "</td>\n        <td>").concat(serie.name, "</td>\n        <td>").concat(serie.channel, "</td>\n        <td>").concat(serie.seasons, "</td>");
+        trElement.innerHTML = "<th scope=\"row\">".concat(serie.orden, "</th>\n        <td style=\"color: blue; text-decoration: underline;\">").concat(serie.name, "</td>\n        <td>").concat(serie.channel, "</td>\n        <td>").concat(serie.seasons, "</td>");
         seriesTbody.appendChild(trElement);
         average += serie.seasons;
     }
     seriesTable.appendChild(seriesTbody);
-    var h2Element = document.createElement("h2");
-    h2Element.textContent = "Promedio de temporadas: ".concat(average / datos.length);
-    document.body.appendChild(h2Element);
+    var pElement = document.createElement("p");
+    pElement.textContent = "Promedio de temporadas: ".concat(average / datos.length);
+    averageLabel.appendChild(pElement);
 }
+function mostrarInfo(serie) {
+    var serieInfo = document.getElementById('info');
+    if (serieInfo) {
+        var cardInfo = "<div class=\"card\">\n                            <img src=".concat(serie.image, " class=\"card-img-top\">\n                                <div class=\"card-body\">\n                                <h2 class=\"card-title\">").concat(serie.name, "</h5>\n                                <p class=\"card-text\">").concat(serie.description, "</p>\n                                <a href=\"").concat(serie.link, "\" class=\"btn btn-primary\">Ir a la web</a>\n                            </div>\n                        </div>");
+        serieInfo.innerHTML = cardInfo;
+    }
+}
+var seriesNombres = document.querySelectorAll('#series td:nth-child(2)');
+seriesNombres.forEach(function (nombre, index) {
+    nombre.addEventListener('click', function () {
+        mostrarInfo(Data[index]);
+    });
+});
